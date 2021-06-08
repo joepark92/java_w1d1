df3e581349e5eca0054e8a3e868f4ebb4536e10e
Joe Park <joepark92@gmail.com>
2021-06-08 13:17:09 -0700

    initial commit

---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+
+        FizzBuzz val = new FizzBuzz();
+        
+        System.out.println(val.fizzBuzz(3));
+        System.out.println(val.fizzBuzz(5));
+        System.out.println(val.fizzBuzz(15));
+        System.out.println(val.fizzBuzz(2));
+        System.out.println(val.fizzBuzz(45));
+    }
+}
