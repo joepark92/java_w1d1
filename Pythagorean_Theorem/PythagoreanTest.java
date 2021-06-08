92d664baec27e21bb2f7aea9d820a141cb426b66
Joe Park <joepark92@gmail.com>
2021-06-08 09:11:07 -0700

    initial commit

---
+++ Pythagorean_Theorem/PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean test = new Pythagorean();
+        System.out.println(test.calculateHypotenuse(2,6));
+        System.out.println(test.calculateHypotenuse(6,9));
+        System.out.println(test.calculateHypotenuse(7,12));
+    }
+}
